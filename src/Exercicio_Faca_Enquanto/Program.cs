490c4b009ae685a3da7473cbf54098586e322189
RaulORios <raul.orios89@outlook.com>
2021-08-05 12:09:11 -0300

    Program.cs adicionado

---
+++ src/Exercicio_Faca_Enquanto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Faca_Enquanto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double C, F;
+            char repetir;
+
+            do
+            {
+                Console.Write("Digite a temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                F = (9.0 * C) / 5.0 + 32.0;
+
+                Console.WriteLine($"Equivalente em Fahrenheit: {F.ToString("F1", CultureInfo.InvariantCulture)}");
+
+                Console.Write("Deseja repetir (S/N)? ");
+                repetir = char.Parse(Console.ReadLine().ToUpper());
+            } while (repetir == 'S');
+
+            Console.ReadKey();
+                       
+        }
+    }
+}
